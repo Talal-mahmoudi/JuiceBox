62119bf3c2060e56f58cd3991a96945a20a31eb0
Talal-mahmoudi <tkm16c@my.fsu.edu>
2023-03-19 20:36:50 -0400

    attempting to deploy

--- db/index.js
+++ db/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const { Client } = require('pg');
-const client = new Client('postgres://localhost:5432/juicebox-dev');
+// const client = new Client('postgres://localhost:5432/juicebox-dev');
+const client = new Client(process.env.DATABASE_URL);
 
 async function createUser({ 
     username, 
